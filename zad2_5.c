54b9f4eed514fc13150cee68fbdb636872bede41
Michał Nowakowski <m98.nowakowski@gmail.com>
2021-10-23 15:53:04 +0200

    Wykonanie zadania rozdział 2 zadanie 5

---
+++ zad2_5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int liczba = 10;
+    int podwojenie = liczba*2;
+    int kwadrat = podwojenie * podwojenie;
+    printf("Liczba: %d\nPodwojenie: %d\nKwadrat podwojenia: %d", liczba, podwojenie, kwadrat);
+    return 0;
+}
