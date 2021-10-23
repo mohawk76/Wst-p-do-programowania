3859ad92590ffa2bdae5f8b4e92ae08dd546e21e
Michał Nowakowski <m98.nowakowski@gmail.com>
2021-10-23 15:49:14 +0200

    Wykonanie zadania rozdział 2 zadanie 4

---
+++ zad2_4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void call_jan(){
+    printf("Panie Janie!\n");
+}
+
+void hey_wake_up(){
+    printf("Rano wstan!\n");
+}
+
+int main()
+{
+    call_jan();
+    call_jan();
+    hey_wake_up();
+    return 0;
+}
