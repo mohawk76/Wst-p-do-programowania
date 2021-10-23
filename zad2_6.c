909350c70d8653af222eebc8b94135c51151d76c
Michał Nowakowski <m98.nowakowski@gmail.com>
2021-10-23 15:55:18 +0200

    Wykonanie zadania rozdział 2 zadanie 6

---
+++ zad2_6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void smile(){
+    printf("Usmiech!");    
+}
+
+int main()
+{
+    smile();
+    smile();
+    smile();
+    printf("\n");
+    smile();
+    smile();
+    printf("\n");
+    smile();
+    return 0;
+}
